65ecbfcc70e4d8c0645809ce07cbbe5f087f4ebc
lijie <lijie@huobi.com>
2020-04-09 00:06:14 +0800

    update

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -2,17 +2,13 @@ export default (state = {count: 0}, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return {
-                state,
-                ...{
-                    count: state.count + 1
-                }
+                ...state,
+                count: state.count + 1
             };
         case 'DECREMENT':
             return {
-                state,
-                ...{
-                    count: state.count - 1
-                }
+                ...state,
+                count: state.count - 1
             };
         default:
             return state
